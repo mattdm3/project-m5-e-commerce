810e46dc117457635b1ff1fe79c0550b9bc42fa8
hjweinreb <60324170+hjweinreb@users.noreply.github.com>
2020-04-10 11:20:32 -0400

    garbage

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -3,8 +3,8 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 // // ------------- COMPONENTS -------------
-import Navbar from '../HomePage/Navbar'
-import ItemGrid from '../HomePage/ItemGrid'
+//import Navbar from '../HomePage/Navbar'
+//import ItemGrid from '../HomePage/ItemGrid'
 // //---------------------------------------
 // =======
 import GlobalStyles from '../GlobalStyles';
@@ -44,7 +44,7 @@ function App() {
       </Router>
 
     </>
-// >>>>>>> master
+    // >>>>>>> master
   )
 }
 
