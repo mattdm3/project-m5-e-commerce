99bedf129101256a0572ebe5743814adabdc2b69
Matteo Mortelliti <mortellitimatteo@gmail.com>
2020-04-11 09:48:58 -0400

    added search bar

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -20,14 +20,18 @@ const Sidebar = () => {
 
 
     return (
-        <div>
+        <SidebarContainer>
+            <StyledForm>
+                <StyledInput placeholder="Search Products..." />
+                <button>Go</button>
+            </StyledForm>
             {categories.map(category => {
                 return (
                     <NavigationLink to={`/category/${category}`}><div>{category}</div></NavigationLink>
                 )
             })}
 
-        </div>
+        </SidebarContainer>
     )
 }
 
@@ -37,5 +41,34 @@ const NavigationLink = styled(NavLink)`
     text-decoration: none;
     color: black;
 `
+
+const StyledForm = styled.form`
+    position: relative; 
+    margin-bottom: 80px; 
+    
+    button {
+        border: none; 
+        background: none; 
+        font-size: 1rem; 
+        position: absolute;
+        right: 5px;
+        top: 11px;   
+    }
+`
+
+const StyledInput = styled.input`
+ width: 100%; 
+ height: 45px; 
+ font-size: .9rem;
+ border: none; 
+ background: #EEEEEE; 
+ padding: 0 5px; 
+
+`
+
+const SidebarContainer = styled.div`
+    width: 25%; 
+`
+
 
 export default Sidebar;
