c5dc72592c74c0c8b5f7576639ad3554fc475211
Matteo Mortelliti <mortellitimatteo@gmail.com>
2020-04-11 20:32:46 -0400

    nav links fixed, hamburger menu started

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -22,70 +22,69 @@ import Seller from "./Seller";
 const Navbar = () => {
     let history = useHistory();
 
+    const [navbar, setNavbar] = React.useState(false);
+
+    function toggleNavbar() {
+
+        if (navbar) {
+            setNavbar(false)
+        } else if (!navbar) {
+            setNavbar(true)
+        }
+
+    }
+
 
     return (
         <Router>
             <StyledNav>
-                <NavigationLink to="/">
-
-                    {/* <Link to="/"> */}
-                    <button onClick={() => history.push(`/`)}>
-                        <Logo>
-                            <FaDiceSix size={20} style={{ marginRight: "5px", color: "#FF4F40" }} />
-                            <h3> Six Tech Gear</h3>
-                        </Logo>
-                    </button>
-
+                <NavigationLink exact to="/">
+                    <Logo onClick={() => history.push('/')} >
+                        <FaDiceSix size={20} style={{ marginRight: "5px", color: "#FF4F40" }} />
+                        <h3>Six Tech Gear</h3>
+                    </Logo>
                 </NavigationLink>
+
+                <HiddenNavigation style={(navbar) ? { transform: "translateX(-500px)" } : {
+                    transform: "translateX(-1000px)"
+                }}>
+                    <OverlayMenu>
+                        <Link onClick={toggleNavbar} to="/"><li>Shop</li></Link>
+                        <Link to="/projects"><li>Sellers</li></Link>
+                        <Link to="/services"><li>Cart</li></Link>
+                        <Link to="/contact"><li>Contact</li></Link>
+                    </OverlayMenu>
+
+                </HiddenNavigation>
+
+
                 <StyledUl>
                     <NavList>
-                        <NavigationLink to="/">Shop</NavigationLink>
+                        <NavigationLink exact to="/">Shop</NavigationLink>
                     </NavList>
                     <NavList>
-                        <NavigationLink to="/Seller">Seller</NavigationLink>
+                        <NavigationLink exact to="/sellers">Sellers</NavigationLink>
                     </NavList>
                     <NavList>
-                        <NavigationLink to="/Cart">Cart</NavigationLink>
+                        <NavigationLink exact to="/cart">Cart</NavigationLink>
                     </NavList>
                     <NavList>
-                        <NavigationLink to="/Contact">Contact</NavigationLink>
+                        <NavigationLink exact to="/contact">Contact</NavigationLink>
                     </NavList>
-
-
-                    {/* </Link> */}
-
-                    {/* <Link to="/"><NavList>Shop</NavList></Link> */}
-                    {/* <Link to="/sellers"><NavList>Sellers</NavList></Link>
-                    <Link to="/Cart"><NavList>Cart</NavList></Link>
-                    <Link to="/Contact"><NavList>Contact</NavList></Link> */}
-
                 </StyledUl>
-                <Hamburger>&#9776;</Hamburger>
+                <Hamburger onClick={toggleNavbar}>&#9776;</Hamburger>
             </StyledNav>
 
             <ContentContainer>
                 <Switch>
-
-                    {/* <Route exact path="/"> */}
-
-                    {/* <Route path="/"> */}
-
-                    {/* <Shop />  not made yet*/}
-                    {/* </Route> */}
-
                     <Route path="/Seller">
                         <Seller />
-                        {/* <Seller />  not made yet*/}
                     </Route>
-
                     <Route path="/Cart">
                         <Cart />
                     </Route>
-
                     <Route path="/Contact">
-
                         <Contact />
-
                     </Route>
                 </Switch>
             </ContentContainer>
@@ -118,27 +117,59 @@ const StyledNav = styled.div`
     width: 100%;
     align-items: center;
     padding: 50px 50px;
-    /* position: fixed;  */
-    /* background: #FAFAFA; */
     transition-duration: .4s;
-
-    /* h4, h3 {
-                    padding: 0 40px;
-    } */
-
-    /* @media screen and (min-width: 768px) {
-                    padding: 15px 0;
-    }
-    @media screen and (min-width: 992px) {
-                    padding: 15px 0;
-    } */
-
 
 `
 
+const HiddenNavigation = styled.div`
+    position: fixed; 
+    width: 100%;
+    height: 100vh; 
+    transition-duration: .7s;
+    top: 0; 
+    z-index: 100; 
+    background-color: #007C89;
+    
+
+`
+
+const OverlayMenu = styled.ul`
+    display: flex; 
+    align-items: flex-end;
+    text-align: center;
+    flex-direction: column;
+    justify-content: center;
+    position: fixed;
+    top: 0; 
+    background-color: #333333;
+    color: white; 
+    margin: 0; 
+    padding: 0;
+    width: 100%;
+    height: 100vh; 
+    z-index: 100; 
+    font-size: 1.2rem;
+    /* opacity: .9; */
+
+
+    li {
+        list-style: none;
+        font-weight: 700;
+        color: white; 
+    
+        margin: 5px 0; 
+        padding: 15px; 
+        cursor: pointer;
+        border-bottom: 3px solid transparent;
+
+        &:hover {
+        border-bottom: 3px solid #FDC600;
+        }
+    }
+
+`
 
 const StyledUl = styled.ul`
-    /* padding: 0 40px; */
     display: flex;
     justify-content: space-evenly;
     display: none;
