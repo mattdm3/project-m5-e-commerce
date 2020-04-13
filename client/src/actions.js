b16227a953bccf56a4ba326208d8fdd3ca44de8a
Myra-Grace <59889116+myra-grace@users.noreply.github.com>
2020-04-13 15:03:24 -0400

    stock data

--- client/src/actions.js
+++ client/src/actions.js
@@ -18,6 +18,15 @@ export const clearCart = () => ({
     type: 'CLEAR_CART',
 });
 
+export const updateStock = (cartState) => ({
+    type: 'UPDATE_STOCK',
+    cartState,
+});
+
+export const clearInventoryReducer = () => ({
+    type: 'CLEAR_INVENTORY_REDUCER',
+});
+
 
 
 //ITEM DATA ACTIONS - PAGINATION (9 ITEMS AT A ITME)
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,6 +3,7 @@ import items from './items-reducer';
 // <<<<<<< searchBar-2-manny
 // // import cart from './cart-reducer';
 import dataItems from './dataItems-reducer';
+import inventoryReducer from './inventory-reducer';
 
 // export default combineReducers({ items, cart, dataItems })
 
@@ -11,6 +12,7 @@ import cartReducer from './cart-reducer';
 export default combineReducers({
     dataItems,
     cartState: cartReducer,
+    inventoryReducer,
     items,
 });
 // >>>>>>> master
