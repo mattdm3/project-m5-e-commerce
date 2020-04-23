237320b33fa93803caabc2f249f1321ede2781d9
Matteo Mortelliti <mortellitimatteo@gmail.com>
2020-04-23 13:56:37 -0400

    new build

---
+++ build/precache-manifest.206c1c611a7430e1c0eb844345f16152.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3fcf053cf7ee11b19a502398c9a932e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ceda65e1635752758c48",
+    "url": "/static/js/2.96916812.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.96916812.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cedeba359121180f3a36",
+    "url": "/static/js/main.acd0d78a.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "f1028cf4712446771798a45cbbd09cf0",
+    "url": "/static/media/entertain-image1.f1028cf4.jpg"
+  },
+  {
+    "revision": "7a8d0608821a22c244196f1efe006d30",
+    "url": "/static/media/entertain-image2.7a8d0608.png"
+  },
+  {
+    "revision": "c3182ba7e68044bc0d277479a6c77aa4",
+    "url": "/static/media/fitness-image.c3182ba7.png"
+  },
+  {
+    "revision": "485394b602fe79431222bec31bcb8bbb",
+    "url": "/static/media/fitness-image1.485394b6.jpg"
+  },
+  {
+    "revision": "1357022a0547fb66bf4c6be9a6c2a9ca",
+    "url": "/static/media/gaming-logo.1357022a.png"
+  },
+  {
+    "revision": "966a0b55a766d7fa7a5df3d807633e16",
+    "url": "/static/media/gaming2.966a0b55.jpg"
+  },
+  {
+    "revision": "8ca9ba036396611057c4e66e17ff7a0f",
+    "url": "/static/media/industrial-image.8ca9ba03.png"
+  },
+  {
+    "revision": "8c2b13a749a8e36acab7af6f86d81ed9",
+    "url": "/static/media/industrial2.8c2b13a7.jpg"
+  },
+  {
+    "revision": "4373c30be0fedd598c03128a74bd8f6e",
+    "url": "/static/media/lifestyle-logo-2.4373c30b.jpg"
+  },
+  {
+    "revision": "b06b23d567c00a5cc49ddf6b37b8c9b0",
+    "url": "/static/media/lifestyle-logo.b06b23d5.png"
+  },
+  {
+    "revision": "09e9342c55420168dc5fb48d4c5fc11a",
+    "url": "/static/media/lifestyle-logo1.09e9342c.jpg"
+  },
+  {
+    "revision": "c6d16ed6a02bc3554b746e8023f10dc7",
+    "url": "/static/media/medical-img.c6d16ed6.png"
+  },
+  {
+    "revision": "d87848b5f2e3a173af1b855d8cf8d476",
+    "url": "/static/media/pet-image.d87848b5.png"
+  },
+  {
+    "revision": "7b503af13a9c364778cd0e5c87643dd9",
+    "url": "/static/media/pet-image1.7b503af1.jpg"
+  },
+  {
+    "revision": "f62aa7acdb2e8f9b9fea6c8039655137",
+    "url": "/static/media/shop-image.f62aa7ac.jpg"
+  }
+]);
